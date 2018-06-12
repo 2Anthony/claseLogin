07b62d1914a596f7410477cb97102bd8bfd8d11f
2Anthony <i70213570@continental.edu.pe>
2018-06-11 19:17:17 -0500

    splashScreen

--- src/frutapp/App1/MainActivity.cs
+++ src/frutapp/App1/MainActivity.cs
@@ -6,7 +6,7 @@ using Android.Support.V7.App;
 
 namespace App1
 {
-    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme")]
     public class MainActivity : AppCompatActivity
     {
         Button btnAuth;
